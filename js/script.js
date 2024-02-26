5979851d23dd2660ca6b085be282649b0a8e9378
nvest <nicole.marie.vest@gmail.com>
2024-02-25 19:55:02 -0600

    showPage function

--- js/script.js
+++ js/script.js
@@ -10,21 +10,50 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
+const itemsPerPage = 9;
 
 
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
+---
+Figure out which 9 students the page should show
+Build out their list items, and insert them into the html
 */
-
+function showPage(list, page) {
+   const start = (page * itemsPerPage) - itemsPerPage;
+   const end = page * itemsPerPage;
+   const pageList = document.querySelector("ul.student-list");
+   pageList.innerHTML = '';
+   for ( let i = 0; i < list.length; i++ ) {
+     if (i >= start && i < end) {
+      const student = `
+         <li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>
+      `;
+      pageList.insertAdjacentHTML('beforeend', student);
+     }
+   }
+}
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
 
+}
 
 
 // Call functions
+showPage(data, 1)
